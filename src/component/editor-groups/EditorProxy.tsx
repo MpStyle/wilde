034971e1d9df628375e76150c8e7278588e5ef05
Michele Pagnin <MpStyle@users.noreply.github.com>
2024-01-02 19:00:57 +0100

    Added image viewer for .jpg, .png and .ico files

--- src/component/editor-groups/EditorProxy.tsx
+++ src/component/editor-groups/EditorProxy.tsx
@@ -1,10 +1,19 @@
-import React, {Fragment, FunctionComponent} from "react";
+import React, {FunctionComponent} from "react";
 import {TextEditor} from "./editor/TextEditor";
+import {FileUtils} from "../../book/FileUtils";
+import {ImageViewer} from "./editor/ImageViewer";
 
 export const EditorProxy: FunctionComponent<EditorProxyProps> = props => {
-    return <Fragment>
-        <TextEditor handle={props.handle}/>
-    </Fragment>;
+    const extension=FileUtils.getExtension(props.handle.name);
+
+    switch(extension){
+        case 'png':
+        case 'jpg':
+        case 'ico':
+            return <ImageViewer handle={props.handle} />
+        default:
+            return <TextEditor handle={props.handle}/>
+    }
 }
 
 export interface EditorProxyProps {
---
+++ src/component/editor-groups/editor/ImageViewer.tsx
@@ -0,0 +1,36 @@
+import {FunctionComponent, useEffect, useState} from "react";
+import {Box} from "@mui/material";
+
+export const ImageViewer:FunctionComponent<ImageViewerProps>=props=>{
+    const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        const loadContent = async () => {
+            const file = await props.handle.getFile();
+            const fileContent = await file.arrayBuffer();
+            const blob = new Blob( [ fileContent ], { type: "image/jpeg" } );
+            const urlCreator = window.URL || window.webkitURL;
+            const imageUrl = urlCreator.createObjectURL( blob );
+
+            setImageUrl(imageUrl);
+        }
+
+        if (imageUrl === undefined) {
+            loadContent();
+        }
+    }, [imageUrl])
+
+    return <Box sx={{
+                    height: `calc(100% - 48px - 8px)`,
+                    m: 0,
+                    pt: 1,
+                    overflow: 'scroll'
+                }}>
+        {imageUrl && <img src={imageUrl} alt={props.handle.name} />}
+        {!imageUrl && <div>Loading...</div>}
+    </Box>;
+}
+
+export interface ImageViewerProps {
+    handle: FileSystemFileHandle;
+}
