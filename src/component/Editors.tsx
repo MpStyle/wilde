c1b23ea686d0d96307873d82ade74bb3ea63bd92
Michele Pagnin <MpStyle@users.noreply.github.com>
2024-01-03 09:24:54 +0100

    Changed logo size using breakpoint when no editors are shown

--- src/component/Editors.tsx
+++ src/component/Editors.tsx
@@ -1,14 +1,31 @@
-import {Box} from "@mui/material";
+import {Box, useTheme} from "@mui/material";
 import {FunctionComponent, PropsWithChildren} from "react";
 
 export const Editors: FunctionComponent<PropsWithChildren<EditorsProps>>= props => {
+    const theme=useTheme();
+
     return <Box id='Editors'
                 sx={{
                     flexGrow: 1,
                     height: '100%',
                     overflow: 'hidden',
                     backgroundImage: props.showBackground ? 'url("images/wilde-logo.png")' : null,
-                    backgroundSize: '25%',
+                    backgroundSize: '75%',
+                    [theme.breakpoints.up('sm')]: {
+                        backgroundSize: '50%',
+                    },
+                    [theme.breakpoints.up('md')]: {
+                        backgroundSize: '45%',
+                    },
+                    [theme.breakpoints.up('lg')]: {
+                        backgroundSize: '35%',
+                    },
+                    [theme.breakpoints.up('lg')]: {
+                        backgroundSize: '25%',
+                    },
+                    [theme.breakpoints.up('xl')]: {
+                        backgroundSize: '15%',
+                    },
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat'
                 }}>
