3b5405f799df3d8666a1473449f76393c29b8b2d
Michele Pagnin <MpStyle@users.noreply.github.com>
2024-01-04 20:07:40 +0100

    Added image information in image viewer

--- src/component/editor-groups/editor/ImageViewer.tsx
+++ src/component/editor-groups/editor/ImageViewer.tsx
@@ -1,36 +1,71 @@
-import {FunctionComponent, useEffect, useState} from "react";
+import {Fragment, FunctionComponent, useEffect, useState} from "react";
 import {Box} from "@mui/material";
+import {ByteUtils} from "../../../book/ByteUtils";
 
-export const ImageViewer:FunctionComponent<ImageViewerProps>=props=>{
-    const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
+export const ImageViewer: FunctionComponent<ImageViewerProps> = props => {
+    const [state, setState] = useState<ImageViewerState | undefined>(undefined);
 
     useEffect(() => {
-        const loadContent = async () => {
+        const loadFileInfo = async () => {
             const file = await props.handle.getFile();
             const fileContent = await file.arrayBuffer();
-            const blob = new Blob( [ fileContent ], { type: "image/jpeg" } );
+            const blob = new Blob([fileContent], {type: "image/jpeg"});
             const urlCreator = window.URL || window.webkitURL;
-            const imageUrl = urlCreator.createObjectURL( blob );
+            const imageUrl = urlCreator.createObjectURL(blob);
 
-            setImageUrl(imageUrl);
+            const img = new Image();
+            img.onload = () => {
+                setState({
+                    imageUrl,
+                    fileSize: file.size,
+                    width: img.width,
+                    height: img.height
+                });
+            };
+            img.src = imageUrl;
         }
 
-        if (imageUrl === undefined) {
-            loadContent();
+        if (state === undefined) {
+            loadFileInfo();
         }
-    }, [imageUrl])
+    }, [state])
 
     return <Box sx={{
-                    height: `calc(100% - 48px - 8px)`,
-                    m: 0,
-                    pt: 1,
-                    overflow: 'auto'
-                }}>
-        {imageUrl && <img src={imageUrl} alt={props.handle.name} />}
-        {!imageUrl && <div>Loading...</div>}
+        height: `calc(100% - 48px)`,
+        m: 0,
+        p: 0,
+        overflow: 'hidden'
+    }}>
+        {state && <Fragment>
+            <Box sx={{
+                height: '25px',
+                display: 'flex',
+                alignItems: 'center'
+            }}>
+                <b>Image info</b>: {ByteUtils.format(state.fileSize)} - {state.width}x{state.height}
+            </Box>
+            <Box sx={{
+                height: `calc(100% - 25px)`,
+                m: 0,
+                p: 0,
+                overflow: 'auto',
+                textAlign: 'center'
+            }}>
+                <img src={state.imageUrl}
+                     alt={props.handle.name}/>
+            </Box>
+        </Fragment>}
+        {!state && <div>Loading...</div>}
     </Box>;
 }
 
 export interface ImageViewerProps {
     handle: FileSystemFileHandle;
+}
+
+interface ImageViewerState {
+    imageUrl: string;
+    fileSize: number;
+    height: number;
+    width: number;
 }
