a0ba91375730e3a631c4c46cd5a5bd36850ced62
Michele Pagnin <MpStyle@users.noreply.github.com>
2024-02-25 18:57:49 +0100

    Added "copy name" and "copy path" in context menu of directory explorer

--- src/component/sidebar/DirectoryExplorerContextMenu.tsx
+++ src/component/sidebar/DirectoryExplorerContextMenu.tsx
@@ -1,4 +1,4 @@
-import { Menu, MenuItem } from "@mui/material";
+import { Divider, Menu, MenuItem } from "@mui/material";
 import { FunctionComponent } from "react";
 import { useSelector } from "react-redux";
 import { useWilde } from "../../hook/WildeHook";
@@ -24,40 +24,34 @@ export const DirectoryExplorerContextMenu: FunctionComponent<DirectoryExplorerCo
     } = props;
     const wilde = useWilde();
 
-    if (!data?.fileHandleInfo) {
+    if (!data) {
         return null;
     }
+
+    const menuData = [
+        { label: 'New file...', action: () => wilde.emit(wilde.eventBuilder.onNewFile()), condition: data.fileHandleInfo.handle.kind === 'directory' },
+        { label: 'New folder...', action: () => wilde.emit(wilde.eventBuilder.onNewDirectory()), condition: data.fileHandleInfo.handle.kind === 'directory' },
+        { label: 'Delete', action: () => wilde.emit(wilde.eventBuilder.onDeleteFile(data.fileHandleInfo)), condition: data.fileHandleInfo.handle !== rootDirectory },
+        { type: 'divider' },
+        { label: `Copy ${data.fileHandleInfo.handle.kind} name`, action: () => navigator.clipboard.writeText(data.fileHandleInfo.handle.name) },
+        { label: 'Copy path', action: () => navigator.clipboard.writeText(data.fileHandleInfo.path) },
+    ];
 
     return <Menu open={open}
         onClose={onClose}
         anchorReference="anchorPosition"
-        anchorPosition={
-            data !== null
-                ? { top: data.mouseY, left: data.mouseX }
-                : undefined
-        }>
-        {data.fileHandleInfo.handle.kind === 'directory' &&
-            <MenuItem onClick={() => {
-                onClose();
-                wilde.emit(wilde.eventBuilder.onNewFile());
-            }}>
-                New File...
-            </MenuItem>}
+        anchorPosition={{ top: data.mouseY, left: data.mouseX }}>
 
-        {data.fileHandleInfo.handle.kind === 'directory' &&
-            <MenuItem onClick={() => {
-                onClose();
-                wilde.emit(wilde.eventBuilder.onNewDirectory());
-            }}>
-                New folder...
-            </MenuItem>}
+        {menuData
+            .filter(menu => menu.condition || menu.condition === undefined)
+            .map(menu => {
+                if (menu.type === 'divider') {
+                    return <Divider />
+                }
 
-        {data.fileHandleInfo.handle !== rootDirectory &&
-            <MenuItem onClick={() => {
-                onClose();
-                wilde.emit(wilde.eventBuilder.onDeleteFile(data.fileHandleInfo));
-            }}>
-                Delete
-            </MenuItem>}
+                return <MenuItem onClick={() => { onClose(); menu.action!(); }}>
+                    {menu.label}
+                </MenuItem>;
+            })}
     </Menu>
 }
