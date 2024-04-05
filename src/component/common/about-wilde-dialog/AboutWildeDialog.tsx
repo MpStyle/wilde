69b7b70762b698caa34e3eca5844fbab72dfc7bf
Michele Pagnin <michele.pagnin@carel.com>
2024-04-05 15:07:16 +0200

    Fixed visualization of warning message in AboutWildeDialog component

--- src/component/common/about-wilde-dialog/AboutWildeDialog.tsx
+++ src/component/common/about-wilde-dialog/AboutWildeDialog.tsx
@@ -1,14 +1,24 @@
 import CloseIcon from '@mui/icons-material/Close';
-import { Alert, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Stack, Typography } from "@mui/material";
-import { FunctionComponent, useEffect, useState } from "react";
-import { useWilde } from "../../../hook/WildeHook";
-import { useGetMinorVersionQuery } from "../../../slice/VersionSlice";
-import { t } from 'i18next';
-import { Alarm } from '@mui/icons-material';
+import {
+    Alert,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    IconButton,
+    Stack,
+    Typography
+} from "@mui/material";
+import {FunctionComponent, useEffect, useState} from "react";
+import {useWilde} from "../../../hook/WildeHook";
+import {useGetMinorVersionQuery} from "../../../slice/VersionSlice";
+import {t} from 'i18next';
+import {Alarm} from '@mui/icons-material';
 
 export const AboutWildeDialog: FunctionComponent = () => {
     const [open, setOpen] = useState<boolean>(false);
-    const { data, error, isLoading } = useGetMinorVersionQuery();
+    const {data, error, isLoading} = useGetMinorVersionQuery();
     const wilde = useWilde();
 
     // onShowAboutWilde event listener
@@ -58,8 +68,8 @@ export const AboutWildeDialog: FunctionComponent = () => {
     return <Dialog open={open} onClose={onClose}>
         <DialogTitle>
             <Stack direction="row" alignItems="center">
-                <img src="logo100.png" height="40" />
-                <Typography variant="h3" sx={{ ml: 1.5 }}>{process.env.REACT_APP_NAME}</Typography>
+                <img src="logo100.png" height="40"/>
+                <Typography variant="h3" sx={{ml: 1.5}}>{process.env.REACT_APP_NAME}</Typography>
             </Stack>
         </DialogTitle>
         <IconButton
@@ -71,12 +81,12 @@ export const AboutWildeDialog: FunctionComponent = () => {
                 top: 8,
                 color: (theme) => theme.palette.grey[500],
             }}>
-            <CloseIcon />
+            <CloseIcon/>
         </IconButton>
         <DialogContent>
             <Typography>
                 {data && <span><strong>Version:</strong> {process.env.REACT_APP_VERSION}.{data ?? 0}</span>}
-                {error && <Alert severity='success'>{t("Error while retrieve Wilde version")}</Alert>}
+                {error && <Alert severity='warning' sx={{mb: 2}}>{t("Error while retrieve Wilde version")}</Alert>}
                 {isLoading && <span>{t("Loading version, please wait...")}</span>}
             </Typography>
             {data && <Typography><strong>{t("Date")}:</strong> {formatTimestamp(parseInt(data))}</Typography>}
@@ -84,7 +94,7 @@ export const AboutWildeDialog: FunctionComponent = () => {
         </DialogContent>
         <DialogActions>
             <Button onClick={onClose}
-                aria-label="close">
+                    aria-label="close">
                 {t("Close")}
             </Button>
         </DialogActions>
